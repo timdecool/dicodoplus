da013929e0d3cce4cd600277ff4d9f5c84f636c8
timdecool <tim.decool@gmail.com>
2023-09-24 00:05:50 +0200

    options section

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,13 @@
+// Options
+const optionButtons = document.getElementsByClassName("option");
+const chronoButtons = document.getElementsByClassName("chrono");
+const difficulteButtons = document.getElementsByClassName("difficulte");
+let indices = ["Définitions","Images","Proverbes et citations"];
+let chrono = true;
+let difficulte = "Normal";
+
 // Sections
-const buttons = document.getElementsByClassName("btn");
+const linkButtons = document.getElementsByClassName("link");
 const menu = document.querySelector('.menu');
 const rules = document.querySelector('.rules');
 const options = document.querySelector('.options');
@@ -8,22 +16,58 @@ let activeSection;
 
 const gamewindow = document.querySelector('.game_window');
 
+// OPENING
+setTimeout(() => {
+    gamewindow.classList.toggle('shrunk');
+})
+
 const activeButton = (e) => {
-    for(let i=0; i<buttons.length;i++) {
-        if(buttons[i].classList.contains('active')) {
-            buttons[i].classList.toggle('active');
+    for(let i=0; i<linkButtons.length;i++) {
+        if(linkButtons[i].classList.contains('active')) {
+            linkButtons[i].classList.toggle('active');
         }
-        else if(buttons[i] == e.target && !buttons[i].classList.contains('active')) {
+        else if(linkButtons[i] == e.target && !linkButtons[i].classList.contains('active')) {
                 e.target.classList.toggle('active');
         }
     }
 }
 
-for(let i=0; i<buttons.length; i++) {
-    buttons[i].addEventListener('click', (e) => {
+for(let i=0; i<linkButtons.length; i++) {
+    linkButtons[i].addEventListener('click', (e) => {
         displaySection(e);
         
     });
+}
+
+for(let i=0; i<optionButtons.length; i++) {
+    optionButtons[i].addEventListener('click', (e) => {
+        if(e.target.classList.contains('chrono')) {
+            for(let i=0; i<chronoButtons.length; i++) {
+                chronoButtons[i].classList.toggle('active');
+            }
+            chrono = !chrono;
+        } 
+        else if(e.target.classList.contains('indices')) {
+            if (indices.length > 1 || !e.target.classList.contains('active')) {
+                e.target.classList.toggle('active');
+                if(indices.indexOf(e.target.outerText) > -1) {
+                    indices.splice(indices.indexOf(e.target.outerText),1);
+                } else {
+                    indices.push(e.target.outerText);
+                }
+            }
+        }
+        else if(e.target.classList.contains('difficulte')) {
+            for(let i=0; i<difficulteButtons.length;i++) {
+                if(difficulteButtons[i].classList.contains('active')) {
+                    difficulteButtons[i].classList.toggle('active');
+                }
+            }
+            e.target.classList.toggle('active');
+            difficulte = e.target.outerText;
+        }
+        console.log(indices,chrono,difficulte);
+    })
 }
 
 // Fonction d'affichage des sections
@@ -46,5 +90,5 @@ const displaySection = (e) => {
         }
         menu.classList.toggle('hidden');
         gamewindow.classList.toggle('shrunk')
-    }, 320);
+    }, 300);
 }
